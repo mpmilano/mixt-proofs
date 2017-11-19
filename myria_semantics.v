38ab0098a0f74cc95f85f63d75f631b74f6c7d65
Matthew Milano <milano@cs.cornell.edu>
2017-11-19 15:57:31 -0500

    updates!

--- myria_semantics.v
+++ myria_semantics.v
@@ -13,11 +13,13 @@
 (* begin hide *)
 Require Import Bool Arith String List CpdtTactics.
 Open Scope string_scope.
+Open Scope list_scope.
 
+Import ListNotations.
 
 Definition var := nat.
 
-Inductive binop := Plus | Times | Minus.
+Inductive binop := Plus | Times | Minus | And | Or | Eq.
 
 Inductive exp : Type := 
 | Const : nat -> exp
@@ -25,18 +27,13 @@ Inductive exp : Type :=
 | Binop : exp -> binop -> exp -> exp
 | Tt : exp
 | Ff : exp
-| Eq : exp -> exp -> exp
-| Lt : exp -> exp -> exp
-| And : exp -> exp -> exp
-| Or : exp -> exp -> exp
 | Not : exp -> exp
-| FieldRef : exp -> var -> exp
 | Deref : exp -> exp.
 
 Inductive com : Type := 
 | Skip : com
 | Return : exp -> com
-| Declaration : var -> exp -> com 
+| Declaration : var -> exp -> com -> com 
 | Assign_var : var -> exp -> com
 | Assign_ptr : exp -> exp -> com
 | Seq : com -> com -> com
@@ -46,7 +43,6 @@ Inductive com : Type :=
 Inductive value : Type :=
 | Nat_value : nat -> value
 | Bool_value : bool -> value
-| Record_value : var -> value -> value -> value
 | Remote_value : var -> value.
 Inductive answer : Type :=
 | Value : value -> answer
@@ -58,28 +54,22 @@ Inductive flat_exp : Type :=
 | Flat_Binop : var -> binop -> var -> flat_exp
 | Flat_Tt : flat_exp
 | Flat_Ff : flat_exp
-| Flat_Eq : var -> var -> flat_exp
-| Flat_Lt : var -> var -> flat_exp
-| Flat_And : var -> var -> flat_exp
-| Flat_Or : var -> var -> flat_exp
 | Flat_Not : var -> flat_exp
-| Flat_FieldRef : var -> var -> flat_exp
 (*This one doesn't exist: *)| Flat_Deref : var -> flat_exp.
 
 Inductive flat_com : Type := 
 | Flat_Skip : flat_com
-| Flat_Return : flat_exp -> flat_com
+| Flat_Return : var -> flat_com
 | Flat_Declaration : var -> flat_exp -> flat_com -> flat_com 
 | Flat_Assign_var : var -> var -> flat_com
 | Flat_Assign_ptr : exp -> var -> flat_com
 | Flat_Seq : flat_com -> flat_com -> flat_com
 | Flat_If : var -> flat_com -> flat_com -> flat_com
-| Flat_While : var -> flat_exp -> flat_com -> flat_com.
+| Flat_While : var -> flat_com -> flat_com.
 
 Inductive flat_value : Type :=
 | Flat_Nat_value : nat -> flat_value
 | Flat_Bool_value : bool -> flat_value
-| Flat_Record_value : var -> flat_value -> flat_value -> flat_value
 | Flat_Remote_value : var -> flat_value.
 Inductive flat_answer : Type :=
 | Flat_Value : flat_value -> flat_answer
@@ -140,6 +130,7 @@ Definition eval_binop (b:binop) : nat -> nat -> nat :=
     | Plus => plus
     | Times => mult
     | Minus => minus
+    | _ => (fun _ => (fun _ => 0))
   end.
 
   (** We'll begin by defining a monad where the results are always answers. *)
@@ -156,13 +147,6 @@ Definition eval_binop (b:binop) : nat -> nat -> nat :=
   Notation "x <- c ; f" := (Bind c (fun x => f))
     (right associativity, at level 84, c at next level) : comp_scope.
   Local Open Scope comp_scope.
-
-  Fixpoint lookup_field x e :=
-    match e with
-      | Record_value field_name field_val rest =>
-        if (Nat.eqb x field_name) then ret field_val else lookup_field x rest
-      | _ => TypeError
-    end.
   
 Fixpoint eval_exp (e:exp) (s:state) : answer := 
   match e with 
@@ -171,12 +155,6 @@ Fixpoint eval_exp (e:exp) (s:state) : answer :=
                  | Some e => ret e
                  | None => TypeError
                end
-    | FieldRef e field_name =>
-      match (eval_exp e s) with
-        | Value (Record_value a b c) =>
-          lookup_field field_name (Record_value a b c)
-        | _ => TypeError
-      end
     | Deref e =>
       match (eval_exp e s) with
         | Value (Remote_value x) =>
@@ -194,34 +172,6 @@ Fixpoint eval_exp (e:exp) (s:state) : answer :=
       end
     | Tt => ret (Bool_value true)
     | Ff => ret (Bool_value false)
-    | Eq e1 e2 => fst <- (eval_exp e1 s);
-        snd <- (eval_exp e2 s);
-        match (fst,snd) with
-          | ((Nat_value fst), (Nat_value snd)) =>
-            ret (Bool_value (Nat.eqb fst snd))
-          | _ => TypeError
-        end
-    | Lt e1 e2 => fst <- (eval_exp e1 s);
-        snd <- (eval_exp e2 s);
-        match (fst,snd) with
-          | ((Nat_value fst), (Nat_value snd)) =>
-            ret (Bool_value (Nat.ltb fst snd))
-          |_  => TypeError
-        end
-    | And b1 b2 => fst <- (eval_exp b1 s);
-        snd <- (eval_exp b2 s);
-        match (fst,snd) with
-          | ((Bool_value fst), (Bool_value snd)) =>
-            ret (Bool_value (fst && snd))
-          |_  => TypeError
-        end
-    | Or b1 b2 => fst <- (eval_exp b1 s);
-        snd <- (eval_exp b2 s);
-        match (fst,snd) with
-          | ((Bool_value fst), (Bool_value snd)) =>
-            ret (Bool_value (fst || snd))
-          |_  => TypeError
-        end
     | Not b => tmp <- (eval_exp b s);
         match tmp with | (Bool_value tmp) => ret (Bool_value (negb tmp))
                     | _ => TypeError
@@ -234,7 +184,6 @@ Fixpoint eval_exp (e:exp) (s:state) : answer :=
 Inductive imp_type :=
 | Nat_type : imp_type
 | Bool_type : imp_type
-| Record_type : imp_type
 .
 
 Definition is_declared x s :=
@@ -244,7 +193,6 @@ Definition type_matches ( l r : answer)  :=
   match (l,r) with
     | (Value (Nat_value _), Value (Nat_value _)) => true
     | (Value (Bool_value _), Value (Bool_value _)) => true
-    | (Value (Record_value _ _ _), Value (Record_value _ _ _)) => true
     |  _ => false
   end.
 
@@ -276,12 +224,12 @@ Fixpoint step_com_fn (cmd : com) (s : state) : option (com * state * (option val
                     | Value ans => Some (Skip, s, Some ans)
                     | TypeError => None
                   end
-    | Declaration x e =>
+    | Declaration x e next =>
       match (is_declared x s) with
         | true => None
         | false =>
           match (eval_exp e s) with
-            | Value ans => Some (Skip , (set x ans s), None)
+            | Value ans => Some (next , (set x ans s), None)
             | TypeError => None
           end
       end
@@ -314,28 +262,143 @@ Fixpoint step_com_fn (cmd : com) (s : state) : option (com * state * (option val
     | While condition thn => Some ((If condition (Seq thn cmd) Skip), s, None)
   end.
 
-Eval compute in (step_com_fn (Declaration 0 (And Tt Tt) )
-                             (σ,ρ)
-                ).
-  
-Fixpoint flatten_exp (index : nat) (context : (flat_exp -> flat_com)) (e : exp) : flat_com
-  :=
-  | Var x => context (Flat_Var x)
-  | Not e => match index with
-               | S n => flatten_exp n (fun e' => Flat_Declaration index (Flat_Not e')
-                                                                  (context (Flat_Var index)))
-                                    e
-               | _ => Flat_Skip
-                 end
-  | _ => Flat_Skip
+Fixpoint next_var (index : var) (e : exp) : var
+  := match e with 
+       | Var x => index
+       | Binop e₁ op e₂ =>
+         match (next_var (S index) e₁) with | next => next_var (S next) e₂ end
+       | Not e => next_var (S index) e
+       | Const x => index
+       | Tt => index
+       | Ff => index
+       | Deref e => next_var (S index) e
+     end.
 
-with flatten (index : nat) (c : com) :=
+
+
+Fixpoint flatten_exp (index : var) (e : exp) (context : (flat_exp -> flat_com)) : flat_com
+  := match e with 
+       | Var x => context (Flat_Var x)
+       | Binop e₁ op e₂ =>
+         match (next_var (S index) e₁) with | m => 
+         match (flatten_exp
+                  (S index) e₂
+                  (fun e'' => Flat_Declaration
+                                index e''
+                                (context (Flat_Binop index op m))
+                  )) with
+           | inner_exp =>
+             flatten_exp (S m)
+                         e₁
+                         (fun e' => Flat_Declaration
+                                      m e' inner_exp
+                         )
+         end
+         end
+       | Not e => 
+         flatten_exp (S index) e (fun e' => Flat_Declaration index e' (context (Flat_Not index)))
+       | Const x => context (Flat_Const x)
+       | Tt => context (Flat_Tt)
+       | Ff => context (Flat_Ff)
+       | Deref e => 
+         flatten_exp (S index) e (fun e' => Flat_Declaration index e' (context (Flat_Deref index)))
+     end.
+
+Check nat.
+
+Fixpoint all_declared_vars_surface (c : com) : list var :=
+  match c with
+    | Declaration x _ s => x :: (all_declared_vars_surface s)
+    | Assign_var _ _ => []
+    | Assign_ptr _ _ => []
+    | Seq s₁ s₂ => (all_declared_vars_surface s₁) ++ (all_declared_vars_surface s₂)
+    | If _ _ s => all_declared_vars_surface s
+    | While _ s => all_declared_vars_surface s
+    | Return _ => []
+    | Skip => []
+  end.
+
+Fixpoint all_declared_vars (c : flat_com) : list var :=
+  match c with
+    | Flat_Declaration x _ s => x :: (all_declared_vars s)
+    | Flat_Assign_var _ _ => []
+    | Flat_Assign_ptr _ _ => []
+    | Flat_Seq s₁ s₂ => (all_declared_vars s₁) ++ (all_declared_vars s₂)
+    | Flat_If _ _ s => all_declared_vars s
+    | Flat_While _ s => all_declared_vars s
+    | Flat_Return _ => []
+    | Flat_Skip => []
+  end.
+
+Lemma flatten_exp_index_unique_retc : forall e index,
+                                        index > 0 -> 
+                                   NoDup
+                                     (all_declared_vars
+                                           (flatten_exp index e (fun e' => (Flat_Declaration 0 e' (Flat_Return 0))))).
+Admitted.
+
+Eval compute in (flatten_exp 1 (Binop ((Binop (Const 7) Plus (Const 15) )) Plus ((Binop (Const 7) Plus (Const 15) )) ) (fun e' => (Flat_Declaration 0 e' (Flat_Return 0)))).
+
+Fixpoint step_com_flat (cmd : com_flat) (s : state) : option (com * state * (option value)) :=
+  match cmd with
+    | Seq c1 c2 =>
+      (match (step_com_fn c1 s) with
+              | Some (newcmd,newstate,newret)  =>
+                match newret with
+                  | Some a => None
+                  | None => Some ((Seq newcmd c2), newstate, None)
+                end
+              | None => Some (c2, s, None)
+      end)
+    | Skip => None
+    | Return e => match (eval_exp e s) with
+                    | Value ans => Some (Skip, s, Some ans)
+                    | TypeError => None
+                  end
+    | Declaration x e next =>
+      match (is_declared x s) with
+        | true => None
+        | false =>
+          match (eval_exp e s) with
+            | Value ans => Some (next , (set x ans s), None)
+            | TypeError => None
+          end
+      end
+    | Assign_var x e => if (is_declared x s)
+                             && (type_matches_op (ret_op (get x s)) (Some (eval_exp e s)))
+                        then
+                          match (eval_exp e s) with
+                            | Value ans => Some (Skip , (set_local x ans s), None)
+                            | TypeError => None
+                          end
+                        else None
+    | Assign_ptr x' e => match (eval_exp x' s) with
+                           | Value (Remote_value x) =>
+                             if (is_declared x s)
+                                  && (type_matches_op (ret_op (get x s)) (Some (eval_exp e s)))
+                             then
+                               match (eval_exp e s) with
+                                 | Value ans => Some (Skip , (set_remote x ans s), None)
+                                 | TypeError => None
+                          end
+                             else None
+                           | _ => None
+                       end
+    | If condition thn els =>
+      match (eval_exp condition s) with
+        | Value (Bool_value true) => Some (thn, s, None)
+        | Value (Bool_value false) => Some (els, s, None)
+        | _ => None
+      end
+    | While condition thn => Some ((If condition (Seq thn cmd) Skip), s, None)
+  end.
+
+
+Fixpoint flatten (index : nat) (c : com) : (flat_com, ) :=
   match c with
     | Return e =>
       match (index,e) with
-        | (_,Var x) => Flat_Return (Flat_Var x)
-        | (S index',_) => flatten_exp index'
-                                      (fun e' => (Flat_Declaration index e' (Flat_Return (Flat_Var index)))) e
+        | (S index',_) => flatten_exp index' (fun e' => Flat_Return e') e
         | _ => Flat_Skip
 
       end
@@ -348,3 +411,5 @@ with flatten (index : nat) (c : com) :=
     | While : exp -> com -> com. *)
     | _ => Flat_Skip
   end.
+
+
